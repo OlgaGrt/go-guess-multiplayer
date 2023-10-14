fc9d4e100db801616e708bce8ad2ae0c998f61bf
olgagrts <olagricacueva@gmail.com>
2023-10-14 13:14:45 +0300

    init repository

---
+++ src/main/java/com/example/goguessmultiplayer/GoGuessMultiplayerApplication.java
@@ -0,0 +1,13 @@
+package com.example.goguessmultiplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoGuessMultiplayerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoGuessMultiplayerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/goguessmultiplayer/GoGuessMultiplayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goguessmultiplayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoGuessMultiplayerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
